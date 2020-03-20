4baa53f89ff6208508b17da35256c04467fad820
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-03-20 13:16:18 -0700

    Updates

---
+++ precache-manifest.bdaf15d5b2bec7bdb36fd86072c741f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bcf746fd7870881a72741f634051827",
+    "url": "/marvelteam/index.html"
+  },
+  {
+    "revision": "e69901b1605b1f29df75",
+    "url": "/marvelteam/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c98367119ae8ce4a8bb3",
+    "url": "/marvelteam/static/css/main.c78e2483.chunk.css"
+  },
+  {
+    "revision": "e69901b1605b1f29df75",
+    "url": "/marvelteam/static/js/2.7c8c594d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/marvelteam/static/js/2.7c8c594d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98367119ae8ce4a8bb3",
+    "url": "/marvelteam/static/js/main.8d035d1e.chunk.js"
+  },
+  {
+    "revision": "414917d9093af2ed5e4e",
+    "url": "/marvelteam/static/js/runtime-main.04302a0c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/marvelteam/static/media/SEGA.12a3db8b.woff"
+  }
+]);
